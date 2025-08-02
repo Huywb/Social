93563edd18d727ea692cd04b18fbc68a4c6d9297
huywb <pthhuy1306@gmail.com>
2025-08-02 10:11:38 +0700

    Login page set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,21 @@ import Connection from './pages/Connection'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
+import Message from './pages/Message'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} >
+        <Route path="/" element={<Login />} >
           <Route index element={<Feed />} />
-          <Route path="/message" element={<Message />} />
-          <Route path="/message/:userId" element={<Chatbox />} />
-          <Route path="/connections" element={<Connection />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:profileId" element={<Profile />} />
-          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="message" element={<Message />} />
+          <Route path="message/:userId" element={<Chatbox />} />
+          <Route path="connections" element={<Connection />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
         </Route>
       </Routes>
     </>
