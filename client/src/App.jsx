46650983c532811d36f5628a691dbd9705c97076
huywb <pthhuy1306@gmail.com>
2025-08-02 09:51:42 +0700

    Router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Feed from './pages/Feed'
+import Login from './pages/Login'
+import Chatbox from './pages/Chatbox'
+import Connection from './pages/Connection'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      HEllo
-    </div>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} >
+          <Route index element={<Feed />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/message/:userId" element={<Chatbox />} />
+          <Route path="/connections" element={<Connection />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
